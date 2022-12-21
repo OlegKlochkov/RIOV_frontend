a2cf353c0892477a1fa76753e6b0e6011d213cad
Olekoster <olegk1576@yandex.ru>
2022-12-22 01:13:27 +0300

    added second form and second table

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import Form from './features/form/form';
-import TableFirst from './features/table_first';
+import TableStructure from './features/tableStructure';
 
-let dataSelect = [{ label: 'Форма' }, { label: 'Таблица' }]
+let dataSelect = [{ label: 'Форма 1' }, { label: 'Таблица 1' }, { label: 'Форма 2' }, { label: 'Таблица 2' }]
 function App() {
   const [select, setSelect] = useState('Форма');
   return (
@@ -17,10 +17,16 @@ function App() {
       </div>
       <div className="content_container">
         {
-          select === 'Форма' && <Form />
+          select === 'Форма 1' && <Form path='first/object'/>
         }
         {
-          select === 'Таблица' && <TableFirst />
+          select === 'Таблица 1' && <TableStructure path='first/object' labels={ ['id', 'имя', 'статус'] } keys={ ['id', 'title', 'status'] } />
+        }
+        {
+          select === 'Форма 2' && <Form path='second/object'/>
+        }
+        {
+          select === 'Таблица 2' && <TableStructure path='second/object' labels={ ['id', 'имя', 'статус'] } keys={ ['id', 'title', 'status'] } />
         }
       </div>
     </div>
